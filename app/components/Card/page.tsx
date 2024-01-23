ee066fdde2dda770292e44cd9a6e017b390a98e1
mashuk-tamim <tamimmashuk328@gmail.com>
2024-01-23 13:08:57 +0600

    project done with smooth scroll by lenis

--- app/components/Card/page.tsx
+++ app/components/Card/page.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+"use client";
+import Image from "next/image";
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 interface pageProps {
 	title: string;
@@ -6,6 +9,10 @@ interface pageProps {
 	src: string;
 	link: string;
 	color: string;
+	idx: number;
+	targetScale: number;
+	progress: import("framer-motion").MotionValue<number>;
+	range: number[];
 }
 
 const page: React.FC<pageProps> = ({
@@ -14,11 +21,60 @@ const page: React.FC<pageProps> = ({
 	src,
 	link,
 	color,
+	idx,
+	range,
+    targetScale,
+    progress
 }) => {
-	console.log(title, description, src, link, color);
-    return <div className='h-[100vh] flex justify-center items-center sticky top-0'>
-        <div style={{ backgroundColor: color }} className='h-[300px] w-[300px]'>{title}</div>
-    </div>;
+	const container = useRef(null);
+	const { scrollYProgress } = useScroll({
+		target: container,
+		offset: ["start end", "start start"],
+	});
+	const scale = useTransform(progress, range, [1, targetScale]);
+	const imageScale = useTransform(scrollYProgress, [0, 1], [2, 1]);
+	// console.log(title, description, src, link, color);
+	return (
+		<div
+			className="h-[100vh] flex justify-center items-center sticky top-0"
+			ref={container}
+		>
+			<motion.div
+                style={{
+                    scale: scale,
+					backgroundColor: color,
+					top: `calc(-10% + ${idx * 25}px)`,
+				}}
+				className="w-[900px] h-[500px] relative rounded-3xl p-5"
+			>
+				<h1 className="text-black text-xl font-bold text-center">
+					{title}
+				</h1>
+				<div className="flex items-center justify-between px-10 py-10">
+					<p className=" text-black text-sm w-[40%] pl-10">
+						{description}
+					</p>
+					<div className=" w-[60%] flex justify-end overflow-hidden  rounded-2xl">
+						<motion.div
+							style={{
+								scale: imageScale,
+							}}
+							className="w-full h-full rounded-2xl"
+						>
+							<Image
+								src={`/images/${src}`}
+								alt={title}
+								width={"400"}
+								height={"250"}
+								className="object-cover w-full h-[350px]"
+								priority
+							></Image>
+						</motion.div>
+					</div>
+				</div>
+			</motion.div>
+		</div>
+	);
 };
 
 export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,49 @@
+"use client";
 import Image from "next/image";
-import { projects } from "@/app/components/data"
-import Card from "@/app/components/Card/page"
+import { projects } from "@/app/components/data";
+import Card from "@/app/components/Card/page";
+import { motion, useScroll, motionValue } from "framer-motion";
+import { useEffect, useRef } from "react";
+import Lenis from "@studio-freight/lenis";
 
 export default function Home() {
-    console.log(projects);
-    return <main className="mt-[50vh] mb-[100vh]">
-        {
-            projects.map((project, idx) => {
-                return <Card key={idx} {...project}></Card>
-            })
-        }
-    </main>;
+	const container = useRef(null);
+	const { scrollYProgress } = useScroll({
+		target: container,
+		offset: ["start start", "end end"],
+	});
+	const projectLength = projects.length;
+	// console.log(projectLength);
+	// console.log(projects);
+	useEffect(() => {
+		const lenis = new Lenis();
+
+		lenis.on("scroll", (e) => {
+			console.log(e);
+		});
+
+		function raf(time) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
+	}, []);
+	return (
+		<main ref={container} className="mt-[50vh] mb-[100vh]">
+			{projects.map((project, idx) => {
+				const targetScale = 1 - (projectLength - idx) * 0.05;
+				return (
+					<Card
+						key={idx}
+						idx={idx}
+						{...project}
+						range={[0, 1]}
+						targetScale={targetScale}
+						progress={scrollYProgress}
+					></Card>
+				);
+			})}
+		</main>
+	);
 }
